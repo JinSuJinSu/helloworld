481c7073a16a2f8d2ae3d7bc954b0ea60051326e
JinsuJinsu <hjs928.naver.com>
2022-01-13 16:15:08 +0900

    check hellworld files

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -1,11 +1,19 @@
 package helloworld;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorld {
+	public static int count=10;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloworld");
 
+	}
+	
+	public static void hello() {
+		Map<String, Object> map = new HashMap<>();
 	}
 
 }
